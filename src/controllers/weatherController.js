0fad1269a386bba58e90dcdeaf5d734aa231b691
Chris Greg <christickhubs@gmail.com>
2026-05-05 11:52:10 +0000

    connectRedis to weatherController

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -1,18 +1,33 @@
-import { asyncHandler } from "../middleware/asynHandler.js"
-import * as weatherService from "../services/weatherService.js"
+import { asyncHandler } from "../middleware/asynHandler.js";
+import * as weatherService from "../services/weatherService.js";
+import * as redisService from "../services/redisService.js";
 
 
 export const getWeather = asyncHandler(async (req, res) => {
-    const { city } = req.params;
+    const { city } = req.params.city.toLowerCase();
+    const cachekey = `weather:${city}`
     //logic flow here;
+    // try get data from Redis
+    const cachedData = await redisService.getCache(cachekey)
 
-
+    if (cachedData) {
+        return res.status(200).json({
+            success: true,
+            source: "cache",
+            data: cachedData
+        });
+    }
+    
+    // cache miss; fetch from Visual Crossing
     const data = await weatherService.fetchWeatherData(city)
+
+    // store in Redis for future request (12h expiry)
+    await redisService.setCache(cachekey, freshData)
 
 
     res.status(200).json({ 
         success: true,
         source: "api",
-        data
+        data: freshData
     });
 })
